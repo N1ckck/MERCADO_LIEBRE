bdcef781d620850de5b97a9f07ee5f879d0acbe1
N1ckck <nicolascrisanto65@gmail.com>
2024-01-29 21:51:21 -0300

    Subiendo el ML1-Structure

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express= require('express')
-const app= express
+const app= express()
 const path= require('path')
 const port=3030 
 
 
-app.use(express.static(`public`))
+app.use(express.static('public'))
 app.get('/',(req,res)=>(
     res	.sendfile(path.join(__dirname,'./views/home.html'))
 ))
 
-app.listen(port,()=>console.log(`http:www.mercadoliebre:${port}`))
+app.listen(port,()=>console.log(`http://localhost:${port}`))
 
