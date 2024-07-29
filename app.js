cc2e9e96a0c702ec414d557583e369eddb9e801c
N1ckck <nicolascrisanto65@gmail.com>
2024-07-29 17:11:09 -0300

    se añade los formularios

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const port=3030
 
 app.use(express.static('public'))
 app.get('/',(req,res)=>(
-    res	.sendfile(path.join(__dirname,'./views/home.html'))
+    res	.sendFile(path.join(__dirname,'./views/home.html'))
+))
+app.get('/registro',(req,res)=>(
+    res	.sendFile(path.join(__dirname,'./views/register.html'))
+))
+app.get('/login',(req,res)=>(
+    res	.sendFile(path.join(__dirname,'./views/login.html'))
 ))
 
 app.listen(port,()=>console.log(`http://localhost:${port}`))
